1dfde0aec0b867e767006adaafb96d234783f9b8
Joshua Luo <74257863+cyber-yosh@users.noreply.github.com>
2025-06-04 20:46:37 -0400

    Update generate_updates.py

--- scripts/generate_updates.py
+++ scripts/generate_updates.py
@@ -1,23 +1,19 @@
 import os
 import json
 
-# Get list of .md files in news_updates/
 files = [
     f for f in os.listdir('news_updates')
     if f.endswith('.md')
 ]
 
-# Sort by modification time (most recent first)
 files.sort(
     key=lambda x: os.path.getmtime(f'news_updates/{x}'),
     reverse=True
 )
 
-# Prepare data
 latest = files[0] if files else ''
 data = {'latest_update': f'news_updates/{latest}'}
 
-# Write to updates.json
 with open('updates.json', 'w') as f:
     json.dump(data, f, indent=2)
 
